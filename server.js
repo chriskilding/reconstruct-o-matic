43bd8df5569c9e0864fc3411b54b39e56ef68f35
Christopher Kilding <mr.chris.k@gmail.com>
2013-03-03 20:29:35 +0000

    Can use multiple Redis connections to act as the 'tables'

--- server.js
+++ server.js
@@ -2,32 +2,55 @@
 console.log("Starting up...");
 
 // Uses the socket.io server component (debug output suppressed)
-var io = require('socket.io').listen(3000/*, { log: false }*/);
+var io = require('socket.io').listen(3000 /*, { log: false }*/);
 var wrap = require('wrappers');
 
-var SyncedSkeleton = require('./SyncedSkeleton');
+var SyncedSkeleton = require('lib/SyncedSkeleton');
 
 io.sockets.on('connection', function (socket) {  
   // We need some way to uniquely identify each client
   // socket.io client id is the socket.id - that'll do
   var clientId = socket.id;       
   
-  // Received from a client
-  socket.on('data', function(data) {
-    var combinedSkeleton = SyncedSkeleton.push(data.commonSessionId, clientId, data.skeleton);
+  // Received calibration data from client
+  socket.on('calibrate', function(data) {
+    socket.rooms.forEach(function(roomId) {
+  
+	  var combinedSkeleton = SyncedSkeleton.pushCalibrate(roomId, data);
+	  
+	  if (combinedSkeleton) {
+	    .emit(
+	  }
+  });
     
-    // TODO needs to go to everyone in the common session
-    if (combinedSkeleton) {
-      socket.emit('combinedSkeleton', combinedSkeleton);
-    }
+  // Received 'real' data from a client
+  socket.on('request', function(data) {
+    socket.rooms.forEach(function(roomId) {
+	  var combinedSkeleton = SyncedSkeleton.push(roomId, clientId, data);
+	  
+	  // Needs to go to everyone in the common session
+	  // INCLUDING THE CLIENT THAT SENT THE DATA
+	  if (combinedSkeleton) {
+	    io.sockets.in(roomId).emit('response', combinedSkeleton);
+	  }
+	});
   });
   
-  // Client setting its 'sharing code' to team up with another
-  socket.on('meta', function(data) {
-    SyncedSkeleton.addClientToGroup(data.commonSessionId, clientId);    
+  // Client setting its 'sharing code' to team up with another  
+  socket.on('subscribe', function(data) {
+    socket.join(data.room);
+    SyncedSkeleton.addClientToGroup(data.room, clientId);
+  });
+
+  socket.on('unsubscribe', function(data) {
+    socket.leave(data.room);
   });
   
   socket.on('disconnect', function() {    
     console.log('User disconnected: ' + clientId);  
   });
 });
+
+function isReference(roomId, clientSocket) {
+  return io.sockets.clients(roomId)[0] == clientSocket;
+}
