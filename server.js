dd9186db884c456ad68d7cd608e6aef782db45eb
Christopher Kilding <mr.chris.k@gmail.com>
2013-03-19 18:13:55 +0000

    Refactor some of the server.js code out into another class (more modular for testing, removes the hard dependency on socket.io)

--- server.js
+++ server.js
@@ -3,96 +3,51 @@ console.log("Starting up...");
 
 // Uses the socket.io server component (debug output suppressed)
 var io = require('socket.io').listen(3000, { log: false });
-var _ = require('underscore');
 
 // Ensure Redis is cleaned on startup
 require('redis').createClient(6379, 'localhost').flushall();
 
-var SyncedSkeleton = require('./lib/SyncedSkeleton');
+var SyncedClient = require('./lib/SyncedClient').type;
+
+function isReference(roomId, clientSocketId) {
+  return io.sockets.clients(roomId)[0].id == clientSocketId;
+}
 
 io.sockets.on('connection', function (socket) {  
   // We need some way to uniquely identify each client
   // socket.io client id is the socket.id - that'll do
-  var clientId = socket.id;       
-  
-  // The data conversion function that will be applied
-  // if this is a secondary sensor
-  // defaults to a simple passthrough func at the start
-  var calibrationFunc = function (data) {return data;};
-  
-  // Shortcut to the rooms this client is in
-  var rooms = [];
+  var syncedClient = new SyncedClient(socket.id, isReference);
   
   // Received calibration data from client
   socket.on('calibrate', function(data) {
-		rooms.forEach(function(roomId) {
-			if (isReference(roomId, socket)) {
-				console.log('Reference data for ', roomId);
-
-				SyncedSkeleton.setReferencePoint(roomId, data);
-			}
-			else {
-				SyncedSkeleton.getCalibrationFunc(roomId, data, function(result) {
-				  calibrationFunc = result;
-				});
-			}
-		});
-
+		syncedClient.calibrate(data);
   });
     
   // Received 'real' data from a client
   socket.on('request', function(data) {
-		rooms.forEach(function(roomId) {
-	
-			if (isReference(roomId, socket)) {
-				// Came from a primary sensor
-				SyncedSkeleton.pushReal(roomId, data);
-	
-				// Aaaaand finish the window
-				SyncedSkeleton.finishWindow(roomId, function(reconstructed) {
-					// And tell the room about it
-					// INCLUDING THE CLIENT THAT SENT THE DATA
-					if (reconstructed) {
-						io.sockets.in(roomId).emit('response', reconstructed);
-					}
-				});
-			}
-			else {
-				// Convert data before pushing
-				// (it probably came from a secondary sensor)
-				SyncedSkeleton.pushReal(roomId, calibrationFunc(data));
-			}
-	
+		
+    syncedClient.pushRealData(data, function(roomId, reconstructed) {
+			// And tell the room about it
+			// INCLUDING THE CLIENT THAT SENT THE DATA
+      io.sockets.in(roomId).emit('response', reconstructed);
 		});
+		
   });
   
   // Client setting its 'sharing code' to team up with another  
   socket.on('subscribe', function(roomId) {
   	console.log('client joined room', roomId);
     socket.join(roomId);
-    rooms.push(roomId);
+    syncedClient.joinRoom(roomId);
   });
 
   socket.on('unsubscribe', function(roomId) {
     socket.leave(roomId);
-    
-    // Also strip that value from the 'shortcut' list
-    rooms = _.filter(rooms, function(str) {
-    	return str != roomId;
-    });
-    
-    SyncedSkeleton.clear(roomId);
+    syncedClient.leaveRoom(roomId);
   });
   
   socket.on('disconnect', function() {    
-    console.log('User disconnected: ' + clientId);
-    
-    rooms.forEach(function(roomId) {
-      SyncedSkeleton.clear(roomId);
-    }); 
+    console.log('User disconnected: ' + socket.id);
+    syncedClient.terminate();
   });
 });
-
-function isReference(roomId, clientSocket) {
-  return io.sockets.clients(roomId)[0].id == clientSocket.id;
-}
