d7f530546cb0c8eb54377d0662e5eb722fc5f721
Christopher Kilding <mr.chris.k@gmail.com>
2013-03-20 20:32:58 +0000

    moat rotation tests

--- test/RotationCalibratorTest.js
+++ test/RotationCalibratorTest.js
@@ -9,3 +9,35 @@ test("diffBetweenComponents - identical values = no change", function (assert) {
   equal(result, 0, true);
 });
 
+test("diffBetweenComponents - negative values", function (assert) {
+  var refComponent = -0.5;
+  var otherComponent = -0.5;
+  
+  var result = RotationCalibrator.differenceBetweenComponents(refComponent, otherComponent);
+  
+  equal(result, 0, true);
+});
+
+test("rotationDeltaMatrix - identical values = no change", function (assert) {
+  // a real Kinect data sample
+  var refMatrix = [0.9178107380867004, -0.04468444734811783, -0.3944951295852661, 0.1306413114070892, 0.9723015427589417, 0.1938103586435318, 0.3749079704284668, -0.2294186502695084, 0.8982265591621399];
+  // the same
+  var otherMatrix = [0.9178107380867004, -0.04468444734811783, -0.3944951295852661, 0.1306413114070892, 0.9723015427589417, 0.1938103586435318, 0.3749079704284668, -0.2294186502695084, 0.8982265591621399] ;
+  // no change
+  var expected = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  var actual = RotationCalibrator.rotationDeltaMatrix(refMatrix, otherMatrix);
+  
+  deepEqual(actual, expected, true);
+});
+
+test("rotationDeltaMatrix - all zeroes", function (assert) {
+  // all zeroes should be handled fine
+  var refMatrix = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  // the same
+  var otherMatrix = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  // no change
+  var expected = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  var actual = RotationCalibrator.rotationDeltaMatrix(refMatrix, otherMatrix);
+  
+  deepEqual(actual, expected, true);
+});
