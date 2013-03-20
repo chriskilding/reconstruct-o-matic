2fb2c5d048b15f44bfb5b61bfae9da07b042c5a2
Christopher Kilding <mr.chris.k@gmail.com>
2013-03-20 12:02:04 +0000

    use 3-4-5 rule to test all pythagoras functions

--- test/PositionCalibratorTest.js
+++ test/PositionCalibratorTest.js
@@ -4,12 +4,29 @@ test("first test within module", 1, function (assert) {
     ok(true, "a dummy");
 });
 
-test("pythagorasHyp - works with the 3-4-5 rule", function (assert) {
+test("pythagorasHyp - works with 3-4-5 rule", function (assert) {
   var result = PositionCalibrator.pythagorasHypotenuse(3, 4);
   equal(result, 5, true);
 });
 
-test("pythagorasAdj - works with the 3-4-5 rule", function (assert) {
+test("pythagorasHyp - doesn't crash with negative numbers", function (assert) {
+  var result = PositionCalibrator.pythagorasHypotenuse(-3, -4);
+  equal(result, 5, true);
+});
+
+test("pythagorasAdj - works with 3-4-5 rule", function (assert) {
   var result = PositionCalibrator.pythagorasAdjacent(4, 5);
   equal(result, 3, true);
+});
+
+test("pythagorasAdj - doesn't crash with negative numbers", function (assert) {
+  var result = PositionCalibrator.pythagorasAdjacent(-4, -5);
+  equal(result, 3, true);
+});
+
+test("distanceBetweenSensors - works with 3-4-5 rule", function (assert) {
+  var referencePosition = [3, 3, 3];
+  var otherPosition = [4, 4, 4];
+  var result = PositionCalibrator.distanceBetweenSensors(referencePosition, otherPosition);
+  deepEqual(result, [5, 5, 5], true);
 });
