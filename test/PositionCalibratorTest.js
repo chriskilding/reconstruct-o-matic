3509b347a8ad4f7d76274d8c02f82328c741c058
Christopher Kilding <mr.chris.k@gmail.com>
2013-03-20 11:50:36 +0000

    second test

--- test/PositionCalibratorTest.js
+++ test/PositionCalibratorTest.js
@@ -1,7 +1,5 @@
 QUnit.module("PositionCalibrator");
 
-console.log('poscalibrator', PositionCalibrator);
-
 test("first test within module", 1, function (assert) {
     ok(true, "a dummy");
 });
@@ -9,4 +7,9 @@ test("first test within module", 1, function (assert) {
 test("pythagorasHyp - works with the 3-4-5 rule", function (assert) {
   var result = PositionCalibrator.pythagorasHypotenuse(3, 4);
   equal(result, 5, true);
+});
+
+test("pythagorasAdj - works with the 3-4-5 rule", function (assert) {
+  var result = PositionCalibrator.pythagorasAdjacent(4, 5);
+  equal(result, 3, true);
 });
