06bdf173d5c81c3e1d9bdba7e8924b7f1359b881
Christopher Kilding <mr.chris.k@gmail.com>
2013-03-28 23:40:52 +0000

    Maybe this is the right idea now

--- test/PositionCalibratorTest.js
+++ test/PositionCalibratorTest.js
@@ -8,16 +8,18 @@ test("prepVector - basic test", function (assert) {
   deepEqual(actual.elements, [-1, -1, -1], true);
 });
 
-test("distanceBetweenSensors - works with 3-4-5 rule", function (assert) {
+test("deltaBetweenSensors - if sensors were next to each other", function (assert) {
   var referencePosition = [3, 3, 3];
   var otherPosition = [4, 4, 4];
-  var result = PositionCalibrator.distanceBetweenSensors(referencePosition, otherPosition);
-  deepEqual(result, [5, 5, 5], true);
+  var result = PositionCalibrator.deltaBetweenSensors(referencePosition, otherPosition);
+  // Readings from 'other' would need to be moved 'down' by 1, 'left' by 1 etc.
+  deepEqual(result, [-1, -1, -1], true);
 });
 
-test("distanceBetweenSensors - doesn't crash with negative numbers", function (assert) {
+test("deltaBetweenSensors - doesn't crash with negative numbers", function (assert) {
   var referencePosition = [-3, -3, -3];
   var otherPosition = [-4, -4, -4];
-  var result = PositionCalibrator.distanceBetweenSensors(referencePosition, otherPosition);
-  deepEqual(result, [5, 5, 5], true);
+  var result = PositionCalibrator.deltaBetweenSensors(referencePosition, otherPosition);
+  // Readings from 'other' would need to be moved 'up' by 1, 'right' by 1 etc.
+  deepEqual(result, [1, 1, 1], true);
 });
