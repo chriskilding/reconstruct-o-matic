47eada2bbb3c6fdaa7580998573020dfdadc6496
Christopher Kilding <mr.chris.k@gmail.com>
2013-04-05 00:25:54 +0100

    Port Quaternion unit tests

---
+++ test/QuaternionTest.js
@@ -0,0 +1,197 @@
+// Adapted from the Three.js unit tests for the same module
+
+/**
+ * @author bhouston / http://exocortex.com
+ */
+
+QUnit.module("Quaternion");
+
+var orders = [ 'XYZ', 'YXZ', 'ZXY', 'ZYX', 'YZX', 'XZY' ];
+var eulerAngles = {
+  x: 0.1,
+  y: -0.3,
+  z: 0.25
+};
+var x = 2;
+var y = 3;
+var z = 4;
+var w = 5;
+/*var qSub = function ( a, b ) {
+	var result = Quaternion.create(a);
+	result.copy( a );
+
+	result.x -= b.x;
+	result.y -= b.y;
+	result.z -= b.z;
+	result.w -= b.w;
+
+	return result;
+
+};*/
+
+test("create - no args", function() {
+	var a = Quaternion.create();
+	ok( a.x == 0, true);
+	ok( a.y == 0, true);
+	ok( a.z == 0, true);
+	ok( a.w == 1, true);
+});
+
+test("create - with args", function() {
+	var a = Quaternion.create( x, y, z, w );
+	ok( a.x === x, true);
+	ok( a.y === y, true);
+	ok( a.z === z, true);
+	ok( a.w === w, true);
+});
+
+/*test( "createFromAxisAngle", function() {
+
+	// TODO: find cases to validate.
+	ok( true, true );
+
+	var zero = new THREE.Quaternion();
+
+	var a = new THREE.Quaternion().setFromAxisAngle( new THREE.Vector3( 1, 0, 0 ), 0 );
+	ok( a.equals( zero ), true );
+	a = new THREE.Quaternion().setFromAxisAngle( new THREE.Vector3( 0, 1, 0 ), 0 );
+	ok( a.equals( zero ), true );
+	a = new THREE.Quaternion().setFromAxisAngle( new THREE.Vector3( 0, 0, 1 ), 0 );
+	ok( a.equals( zero ), true );
+
+	var b1 = new THREE.Quaternion().setFromAxisAngle( new THREE.Vector3( 1, 0, 0 ), Math.PI );
+	ok( ! a.equals( b1 ), true );
+	var b2 = new THREE.Quaternion().setFromAxisAngle( new THREE.Vector3( 1, 0, 0 ), -Math.PI );
+	ok( ! a.equals( b2 ), true );
+
+	b1.multiply( b2 );
+	ok( a.equals( b1 ), true );
+});
+
+
+test( "setFromEuler/setEulerFromQuaternion", function() {
+
+	var angles = [ new THREE.Vector3( 1, 0, 0 ), new THREE.Vector3( 0, 1, 0 ), new THREE.Vector3( 0, 0, 1 ) ];
+
+	// ensure euler conversion to/from Quaternion matches.
+	for( var i = 0; i < orders.length; i ++ ) {
+		for( var j = 0; j < angles.length; j ++ ) {
+			var eulers2 = new THREE.Vector3().setEulerFromQuaternion( new THREE.Quaternion().setFromEuler( angles[j], orders[i] ), orders[i] );
+
+			ok( eulers2.distanceTo( angles[j] ) < 0.001, true );
+		}
+	}
+
+});*/
+
+/*test( "createFromEuler", function() {
+	// ensure euler conversion for Quaternion matches that of Matrix4
+  orders.forEach(function (order, index, array) {
+		var q = Quaternion.createFromEuler(eulerAngles, order);
+		var m = new THREE.Matrix4().setRotationFromEuler( eulerAngles, order);
+		var q2 = new THREE.Quaternion().setFromRotationMatrix( m );
+
+		ok( qSub( q, q2 ).length() < 0.001, true );
+	});
+});*/
+
+/*test( "setFromRotationMatrix", function() {
+  orders.forEach(function (order, index, array) {
+		var q = Quaternion.createFromEuler(eulerAngles, order);
+		var m = new THREE.Matrix4().setRotationFromEuler( eulerAngles, order);
+		var q2 = new THREE.Quaternion().setFromRotationMatrix( m );
+
+		ok( qSub( q, q2 ).length() < 0.001, true );
+	});
+});*/
+
+test( "normalize/length/lengthSquared - part 1", function() {
+	var a = Quaternion.create(x, y, z, w);
+
+	ok(Quaternion.length(a) != 1, true);
+	ok(Quaternion.lengthSquared(a) != 1, true);
+	
+  var normA = Quaternion.normalize(a);
+  
+	ok(Quaternion.length(normA) == 1, true);
+	ok(Quaternion.lengthSquared(normA) == 1, true);
+});
+
+test( "normalize/length/lengthSquared - part 1", function() {
+	var a = Quaternion.create(0, 0, 0, 0);
+
+	ok(Quaternion.length(a) == 0, true);
+	ok(Quaternion.lengthSquared(a) == 0, true);
+	
+  var normA = Quaternion.normalize(a);
+  
+	ok(Quaternion.length(normA) == 1, true);
+	ok(Quaternion.lengthSquared(normA) == 1, true);
+});
+
+/*test( "inverse/conjugate", function() {
+	var a = new THREE.Quaternion( x, y, z, w );
+
+	// TODO: add better validation here.
+
+	var b = a.clone().conjugate();
+
+	ok( a.x == -b.x, true );
+	ok( a.y == -b.y, true );
+	ok( a.z == -b.z, true );
+	ok( a.w == b.w, true );
+});
+
+
+test( "multiplyQuaternions/multiply", function() {
+
+	var angles = [ new THREE.Vector3( 1, 0, 0 ), new THREE.Vector3( 0, 1, 0 ), new THREE.Vector3( 0, 0, 1 ) ];
+
+	var q1 = new THREE.Quaternion().setFromEuler( angles[0], "XYZ" );
+	var q2 = new THREE.Quaternion().setFromEuler( angles[1], "XYZ" );
+	var q3 = new THREE.Quaternion().setFromEuler( angles[2], "XYZ" );
+
+	var q = new THREE.Quaternion().multiplyQuaternions( q1, q2 ).multiply( q3 );
+
+	var m1 = new THREE.Matrix4().setRotationFromEuler( angles[0], "XYZ" );
+	var m2 = new THREE.Matrix4().setRotationFromEuler( angles[1], "XYZ" );
+	var m3 = new THREE.Matrix4().setRotationFromEuler( angles[2], "XYZ" );
+
+	var m = new THREE.Matrix4().multiplyMatrices( m1, m2 ).multiply( m3 );
+
+	var qFromM = new THREE.Quaternion().setFromRotationMatrix( m );
+
+	ok( qSub( q, qFromM ).length() < 0.001, true );
+});
+
+test( "multiplyVector3", function() {
+	
+	var angles = [ new THREE.Vector3( 1, 0, 0 ), new THREE.Vector3( 0, 1, 0 ), new THREE.Vector3( 0, 0, 1 ) ];
+
+	// ensure euler conversion for Quaternion matches that of Matrix4
+	for( var i = 0; i < orders.length; i ++ ) {
+		for( var j = 0; j < angles.length; j ++ ) {
+			var q = new THREE.Quaternion().setFromEuler( angles[j], orders[i] );
+			var m = new THREE.Matrix4().setRotationFromEuler( angles[j], orders[i] );
+
+			var v0 = new THREE.Vector3(1, 0, 0);
+			var qv = v0.clone().applyQuaternion( q );
+			var mv = v0.clone().applyMatrix4( m );
+		
+			ok( qv.distanceTo( mv ) < 0.001, true );
+		}
+	}
+
+});*/
+
+test("equals", function() {
+	var a = Quaternion.create( x, y, z, w );
+	var b = Quaternion.create( -x, -y, -z, -w );
+	
+	ok( a.x != b.x, true );
+	ok( a.y != b.y, true );
+
+	ok(!Quaternion.equals(a, b), true );
+	ok(!Quaternion.equals(b, a), true );
+
+});
