edae29a3a0696d8614fcd9561dfddc61f7c7752b
Christopher Kilding <mr.chris.k@gmail.com>
2013-05-06 01:18:33 +0100

    more ClientSkeleton tests

--- test/ClientSkeletonTest.js
+++ test/ClientSkeletonTest.js
@@ -7,7 +7,8 @@ var _ = require("underscore");
 
 var room;
 
-var clientId = "abc123";
+var client1 = "abc123";
+var client2 = "gef456";
 
 QUnit.module("ClientSkeleton", {
     setup: function () {
@@ -22,25 +23,52 @@ test("CS - basic config checks", 1, function (assert) {
     assert.deepEqual(Object.keys(room.clients), [], true);
 });
 
-test("addClient - add 1 client", 1, function (assert) {
-    var returned = room.addClient(clientId);
+test("addClient - add 1 client", 2, function (assert) {
+    var returned = room.addClient(client1);
     
-    var actual = room.clients[clientId];
+    var actual = room.clients[client1];
     
     // Something was added
     assert.equal(actual, returned, true);
+    // It is now the reference client
+    assert.ok(room.isReferenceClient(returned), true);
 });
 
-test("terminate - remove 1 client", 1, function (assert) {
+test("removeClient - remove 1 client", 1, function (assert) {
     var removeMethod = sinon.spy(room, "removeClient");
     
-    var theClient = room.addClient(clientId);
+    var theClient = room.addClient(client1);
     
     // Remove it
     theClient.terminate();
     
-    var actual = room.clients[clientId];
+    var actual = room.clients[client1];
     
     // The client was actually removed
-    assert.ok(removeMethod.calledWith(clientId), true);
+    assert.ok(removeMethod.calledWith(client1), true);
+});
+
+test("referenceClient - with no clients", 1, function (assert) {
+    // There are no clients yet
+    assert.ok(!room.referenceClient, true);
+});
+
+test("isReferenceClient - with 1 client", 1, function (assert) {
+    var addedClient = room.addClient(client1);
+    
+    var actual = room.isReferenceClient(addedClient);
+    
+    // 1st client in is the ref client
+    assert.ok(actual, true);
+});
+
+test("isReferenceClient - with 2 clients", 1, function (assert) {
+    // For a change, add the second one first
+    var c2 = room.addClient(client2);
+    var c1 = room.addClient(client1);
+    
+    var actual = room.isReferenceClient(c2);
+    
+    // client2 is the ref client
+    assert.ok(actual, true);
 });
